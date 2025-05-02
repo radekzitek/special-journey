268acf0e073d10b454166b6ccb92dbe53d0828ec
radekzitek <82449669+radekzitek@users.noreply.github.com>
2025-05-02 08:59:36 +0000

    feat: update AppFooter to include links to external resources

--- frontend/components/AppFooter.vue
+++ frontend/components/AppFooter.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="flex bg-black text-white text-xs p-4">
-      Footer
+      <div class="grid grid-cols-8 gap-4">
+        <div class="font-bold">Links:</div>
+        <div><a href="https://nuxt.com/">Nuxt</a></div>
+        <div><a href="https://ui.nuxt.com/">NuxtUI</a></div>
+        <div><a href="https://nuxt.studio/">Nuxt Studio</a></div>
+        <div><a href="https://tailwindcss.com/">TailwindCSS</a></div>
+        <div><a href="https://iconify.design/">Iconify</a></div>
+      </div>
     </div>
   </template>
   
